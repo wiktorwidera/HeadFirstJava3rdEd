9182b342c70295b042abd879d9509ea25c55d613
Wiktor Widera <Wiktor.Widera@askom.pl>
2023-04-13 14:38:03 +0200

    Initial commit

---
+++ Chapter04/src/Clock.java
@@ -0,0 +1,11 @@
+public class Clock {
+    private String time;
+
+    public void setTime(String t) {
+        time = t;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
---
+++ Chapter04/src/Value.java
@@ -0,0 +1,11 @@
+public class Value {
+    int intValue;
+    public int doStuff(int factor) {
+        if (intValue > 100) {
+            return  intValue * factor;
+        }
+        else {
+            return intValue * (5 - factor);
+        }
+    }
+}
