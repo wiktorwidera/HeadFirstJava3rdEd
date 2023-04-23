e8d903a1ea629d32256eb6ac34f947195c9910c2
wiktorwidera <wiktor.widera@gmail.com>
2023-04-23 09:54:46 +0200

    Part of prep code of the StartupBust class

--- Chapter06/src/StartupBust.java
+++ Chapter06/src/StartupBust.java
@@ -1,2 +1,32 @@
 public class StartupBust {
+    /*
+    GameHelper helper;
+    ArrayList startups;
+    int numOfGuesses = 0;
+
+    void setUpGame() method to create and initialize the Startup objects
+        with names and locations. Display brief instructions to the user
+
+    void startPlaying() method that asks the player for guess and calls the checkUserGuess()
+    method until all the Startup objects are removed from play
+
+    void checkUserGuess() method that loops through all remaining Startup objects and calls
+    each Startup object's checkYourself() method
+
+    void finishGame() method that prints a message about the user's performance,
+    based on how many guesses it took to sink all of the Startup objects
+
+     */
+/*
+    void setUpGame() {
+        create three Startup objects;
+        set a name for each Startup;
+        add the Startups (the ArrayList);
+        repeat with each of the Startup objcts in the startups list:
+            call the placeStartup() method on the helper object, to get a randomly-selected
+                location for this Startup (three cells, vertically or horizontally aligned, on 7x7 grid);
+            set the location for each Startup based on the result of the placeStartup() call
+    }
+
+ */
 }
