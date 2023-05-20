6dc9b5a9dd569e42eeb99003e38d818e94d06726
wiktor.widera <wiktor.widera@gmail.com>
2023-05-20 22:59:26 +0200

    feat: create AnimalTestDrive class example

---
+++ Chapter08/src/AnimalTestDrive.java
@@ -0,0 +1,9 @@
+public class AnimalTestDrive {
+    public static void main(String[] args) {
+        MyAnimalList list = new MyAnimalList();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        list.add(dog);
+        list.add(cat);
+    }
+}
