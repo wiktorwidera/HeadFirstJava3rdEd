364292dfc66b1470e6b1f9e69f856d3021843a90
wiktor.widera <wiktor.widera@gmail.com>
2023-05-25 22:53:07 +0200

    feat: create Duck and StackRef class examples

---
+++ Chapter09/src/Duck.java
@@ -0,0 +1,3 @@
+public class Duck {
+    int size; // Every Duck has a "size" instance variable.
+}
---
+++ Chapter09/src/StackRef.java
@@ -0,0 +1,8 @@
+public class StackRef {
+    public void foof() {
+        barf();
+    }
+    public void barf() {
+        Duck d = new Duck();
+    }
+}
