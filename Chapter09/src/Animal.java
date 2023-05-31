73a580d471adf62e191dc3aa6fc606278bab859e
wiktor.widera <wiktor.widera@gmail.com>
2023-05-31 22:10:54 +0200

    feat: create example of superclass constructors with arguments

--- Chapter09/src/Animal.java
+++ Chapter09/src/Animal.java
@@ -1,5 +1,10 @@
-public class Animal {
-    public Animal() {
-        System.out.println("Making an Animal");
+public abstract class Animal {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public Animal(String theName) {
+        name = theName;
     }
 }
--- Chapter09/src/Hippo.java
+++ Chapter09/src/Hippo.java
@@ -1,5 +1,5 @@
 public class Hippo extends Animal {
-    public Hippo() {
-        System.out.println("Making a Hippo");
+    public Hippo(String name) {
+        super(name);
     }
 }
