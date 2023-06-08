f290390e5be23adf6ae68902b68356d1186219ed
wiktorwidera <wiktor.widera@gmail.com>
2023-06-08 08:20:36 +0200

    feat: create ConstantInit examples

---
+++ Chapter10/src/ConstantInit1.java
@@ -0,0 +1,6 @@
+public class ConstantInit1 {
+    final static int X;
+    static {
+        X = 42;
+    }
+}
---
+++ Chapter10/src/ConstantInit3.java
@@ -0,0 +1,6 @@
+public class ConstantInit3 {
+    public static final double VAL;
+    static {
+        VAL = Math.random();
+    }
+}
