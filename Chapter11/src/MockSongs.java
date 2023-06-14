c4d8b2a34365173301aada0724ca51aca79a18d8
wiktorwidera <wiktor.widera@gmail.com>
2023-06-14 08:58:42 +0200

    feat: create new getSongsV2() method in MockSongs class

--- Chapter11/src/MockSongs.java
+++ Chapter11/src/MockSongs.java
@@ -4,12 +4,22 @@ import java.util.List;
 public class MockSongs {
     public static List<String> getSongStrings() {
         List<String> songs = new ArrayList<>();
-        songs.add("sommersault");
+        songs.add("somersault");
         songs.add("cassidy");
         songs.add("$10");
         songs.add("havana");
         songs.add("Cassidy");
         songs.add("50 Ways");
+        return songs;
+    }
+    public static List<SongV2> getSongsV2() {
+        List<SongV2> songs = new ArrayList<>();
+        songs.add(new SongV2("somersault", "zero 7", 147));
+        songs.add(new SongV2("cassidy", "grateful dead", 158));
+        songs.add(new SongV2("$10", "hitchhiker", 140));
+        songs.add(new SongV2("havana", "cabello", 105));
+        songs.add(new SongV2("Cassidy", "grateful dead", 158));
+        songs.add(new SongV2("50 ways", "simon", 102));
         return songs;
     }
 }
