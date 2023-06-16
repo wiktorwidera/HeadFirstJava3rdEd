8440e4549aa381c671f0bda48928a17643f8d7ed
wiktor.widera <wiktor.widera@gmail.com>
2023-06-16 14:19:41 +0200

    feat: create Jukebox3 class - example of sorting songs by title with compareTo() method

---
+++ Chapter11/src/Jukebox3.java
@@ -0,0 +1,16 @@
+import java.util.Collections;
+import java.util.List;
+
+public class Jukebox3 {
+    public static void main(String[] args) {
+        new Jukebox3().go();
+    }
+
+    public void go() {
+        List<SongV3> songList = MockSongs.getSongsV3();
+        System.out.println(songList);
+
+        Collections.sort(songList);
+        System.out.println(songList);
+    }
+}
