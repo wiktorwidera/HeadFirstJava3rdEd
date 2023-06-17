221718ab11c53c2e247d73ba0195f9fc9a8653f6
wiktor.widera <wiktor.widera@gmail.com>
2023-06-17 23:16:57 +0200

    feat: create ArtistCompare implementation of Comparator

---
+++ Chapter11/src/ArtistCompare.java
@@ -0,0 +1,7 @@
+import java.util.Comparator;
+
+public class ArtistCompare implements Comparator<SongV3> {
+    public int compare(SongV3 one, SongV3 two) {
+        return one.getArtist().compareTo(two.getArtist());
+    }
+}
