9bd38c2adf58a62571043e2b11dc5251a0b42cdb
wiktor.widera <wiktor.widera@gmail.com>
2023-06-18 22:42:03 +0200

    feat: create TitleCompare implementation of Comparator

---
+++ Chapter11/src/TitleCompare.java
@@ -0,0 +1,7 @@
+import java.util.Comparator;
+
+public class TitleCompare implements Comparator<SongV3> {
+    public int compare(SongV3 one, SongV3 two) {
+        return one.getTitle().compareTo(two.getTitle());
+    }
+}
