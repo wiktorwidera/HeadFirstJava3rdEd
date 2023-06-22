7dab0904582dd31feef89904d3ac507303eb0c7f
wiktor.widera <wiktor.widera@gmail.com>
2023-06-22 15:23:08 +0200

    feat: create SongV4 class with overriden hashCode() and equals()

---
+++ Chapter11/src/SongV4.java
@@ -0,0 +1,36 @@
+public class SongV4 implements Comparable<SongV4> {
+    private String title;
+    private String artist;
+    private int bpm;
+
+    SongV4(String title, String artist, int bpm) {
+        this.title = title;
+        this.artist = artist;
+        this.bpm = bpm;
+    }
+
+    public boolean equals(Object aSong) {
+        SongV4 other = (SongV4) aSong;
+        return title.equals(other.getTitle());
+    }
+    public int hashCode() {
+        return title.hashCode();
+    }
+
+    public int compareTo(SongV4 s) {
+        return title.compareTo(s.getTitle());
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public int getBpm() {
+        return bpm;
+    }
+    public String toString() {
+        return title;
+    }
+}
