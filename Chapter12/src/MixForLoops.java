a73140f74658daf41c5d8a7429a579a0a5b4ce3c
wiktor.widera <wiktor.widera@gmail.com>
2023-06-29 20:41:14 +0200

    feat: create MixForLoops example

---
+++ Chapter12/src/MixForLoops.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class MixForLoops {
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(1, 2, 3, 4, 5);
+        String output = "";
+        for (Integer num : nums) {
+            output += nums + " ";
+        }
+        System.out.println(output);
+    }
+}
